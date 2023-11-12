694f8c5a58a8eda38bd1ceea25a8962248c0e54b
Moises Salazar <moises.salazarid@gmail.com>
2023-11-11 20:04:16 -0400

    Finalized the Landing page

--- src/views/LandingPage/LandingPage.jsx
+++ src/views/LandingPage/LandingPage.jsx
@@ -24,43 +24,70 @@ function LandingPage() {
     <div className="wrapper-LandingPage">
       <header>
         <div className="logo">
-          <img src="/svg/site-full-logo.svg"></img>
+          <img src="/svg/full-logo-white.svg"></img>
         </div>
-
         <nav className="actions">
           <a href="/login" className="login-button">ingresar</a>
           <a href="/signup" className="signup-button">
-            UNETE<img src="/svg/white-arrow-right.svg"></img>
+            UNETE<img src="/svg/util/white-arrow-right.svg"></img>
           </a>
         </nav>
       </header>
 
       <main>
         <section className="hero">
-          <div className="hero-description">
-            <div class="description">
+          <div className="hero-desc">
+            <div className="description">
               <span className="slogan">Conecta con tu salud</span>
-
               <span className="title">
-                <strong>HealthBooking</strong> es la web que te facilita la salud
+                HealthBooking es la web que te facilita la salud
               </span>
-
               <span className="comment">Regístrate ya y conecta con tu salud</span>
             </div>
 
             <div className="actions">
-              <a href="/login" className="login-button">ingresar</a>
-              <a href="/signup" className="signup-button">UNETE<img src="/svg/white-arrow-right.svg"></img></a>
+              <a href="/login" className="login">ingresar</a>
+              <a href="/signup" className="signup">UNETE</a>
             </div>
           </div>
-
-          <img src="/img/hero-cover.png"></img>
+          <img className="hero-logo" src="/img/hero-cover.png"></img>
         </section>
-
       </main>
 
       <footer>
+        <div className="menu">
+          <div className="banner">
+            <img src="/svg/full-logo-black.svg"></img>
+            <span><strong>HealthBooking</strong> es un proyecto desarrollado por estudiantes del bootcamp Henry, una academia online que forma desarrolladores web full stack. </span>
+          </div>
 
+          <div className="links-group about">
+            <h1>Sobre este proyecto</h1>
+            <nav>
+              <a href="#"><img src="/svg/util/link-clip.svg"></img>Codigo fuente (frontend)</a>
+              <a href="#"><img src="/svg/util/link-clip.svg"></img>Codigo fuente (backend)</a>
+              <a href="#"><img src="/svg/util/link-clip.svg"></img>Henry Bootcamp</a>
+            </nav>             
+          </div>
+
+          <div className="links-group contact">
+            <h1>Integrantes</h1>
+            <nav>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+              <a href="#"><img src="/svg/brands/linkedin.svg"></img>Moises Salazar<img src="/svg/util/link-clip.svg"></img></a>
+            </nav>              
+          </div>
+        </div>
+
+        <div className="copyright">
+          ©2023 <strong>HealthBooking.</strong> Henry Bootcamp
+        </div>
       </footer>
 
     </div>
