69d48598c84eacc02b5e4a01fe4f59c54e0588ff
Moises Salazar <moises.salazarid@gmail.com>
2023-11-16 14:39:28 -0400

    partial advances in the dashboard layout

---
+++ src/components/layouts/DashboardLayout/DashboardLayout.jsx
@@ -0,0 +1,70 @@
+/*
+
+  About:
+    ...
+
+  @param ...
+
+  Comments:
+    ...
+
+*/
+import "./DashboardLayout.scss";
+import React, {useEffect, useCallback, useRef, useState} from "react";
+
+
+function DashboardLayout() {
+  /* --------------------------------------------------------------------------------*/
+  /* States */
+  /* --------------------------------------------------------------------------------*/
+  const [isLoading, setLoading] = useState(false);
+  const [hasFailed, setFailed] = useState(false);
+  const [state, setState] = useState(0);
+
+  /* --------------------------------------------------------------------------------*/
+  /* Methods*/
+  /* --------------------------------------------------------------------------------*/
+  function function_name(argument) {
+    // body...
+  }
+
+  /* --------------------------------------------------------------------------------*/
+  /* Initialization */
+  /* --------------------------------------------------------------------------------*/
+  useEffect(() => {
+    document.title = 'title';
+  }, []);
+
+  /* --------------------------------------------------------------------------------*/
+  /* Fallbacks */
+  /* --------------------------------------------------------------------------------*/
+  if (isLoading) {
+    return (
+      <div>
+        Cargando...
+      </div>
+    );
+  }
+
+  if (hasFailed) {
+    return (
+      <div>
+        Ha ocurrido un error
+      </div>
+    );
+  }
+
+  /* --------------------------------------------------------------------------------*/
+  /* Main */
+  /* --------------------------------------------------------------------------------*/
+  return (
+    <div className="wrapper-DashboardLayout">
+      <main>
+        <article>
+        </article>
+      </main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
