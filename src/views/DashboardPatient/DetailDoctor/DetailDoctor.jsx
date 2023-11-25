fcdc4cc8468beabf06f72cb1a9f671ee8d46403c
aldo <aldowinarteaga@gmail.com>
2023-11-25 13:14:48 -0600

    detail for doctor style and css file

--- src/views/DashboardPatient/DetailDoctor/DetailDoctor.jsx
+++ src/views/DashboardPatient/DetailDoctor/DetailDoctor.jsx
@@ -28,7 +28,51 @@ const DetailDoctor = () => {
   let specialty = doctor.Specialty;
 
   return (
-      <div className="div-user-information">
+    <div class="container">
+    <div class="inner-container">
+      <div class="profile-section">
+        <div class="name-section">
+          <div class="name">Minatozaki Sana</div>
+        </div>
+        <img class="profile-image" src="https://via.placeholder.com/141x127" />
+        <div class="details-section">
+          <div class="detail-row">
+            <div class="detail-label">Especialidad</div>
+            <div class="detail-value">Pediatria</div>
+          </div>
+          <div class="detail-row">
+            <div class="detail-label">Sure</div>
+            <div class="detail-value">Pediatria</div>
+          </div>
+          <div class="license-section">
+            <div class="license-label">Licencia</div>
+            <div class="data-value">Datos</div>
+          </div>
+          <div class="phone-section">
+            <div class="phone-label">Telefono</div>
+            <div class="data-value">Datos</div>
+          </div>
+          <div class="email-section">
+            <div class="email-label">Email</div>
+            <div class="data-value">Datos</div>
+          </div>
+        </div>
+      </div>
+      <div class="button-section">
+        <div class="back-button">
+          <div class="button-text">Regresa</div>
+        </div>
+        <div class="select-button">
+          <div class="button-text">Seleccionar</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default DetailDoctor;
+/*  <div className="div-user-information">
       <img className="mask-group" alt="Mask group" src={doctor?.profilePicture} />
       <div className="frame">
         <h1 className="text-wrapper">{doctor?.name}</h1>
@@ -74,8 +118,48 @@ const DetailDoctor = () => {
         </div>
      </div>
 
-    </div>
-  )
-}
+    </div> */
 
-export default DetailDoctor;
+
+
+
+    /*  <div style={{width: '100%', height: '100%', paddingTop: 275, paddingBottom: 274.26, paddingLeft: 497, paddingRight: 497, background: '#DAEFEC', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div style={{alignSelf: 'stretch', background: 'white', borderRadius: 8, overflow: 'hidden', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 5, display: 'inline-flex'}}>
+        <div style={{alignSelf: 'stretch', height: 391.74, paddingLeft: 48, paddingRight: 48, paddingTop: 24, paddingBottom: 24, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 10, display: 'flex'}}>
+            <div style={{width: 333, height: 16, paddingLeft: 64, paddingRight: 64, flexDirection: 'column', justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex'}}>
+                <div style={{color: '#352E5B', fontSize: 18, fontFamily: 'DM Sans', fontWeight: '700', wordWrap: 'break-word'}}>Minatozaki Sana</div>
+            </div>
+            <img style={{width: 141, height: 127, borderRadius: 43.63}} src="https://via.placeholder.com/141x127" />
+            <div style={{width: 350, height: 171, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 16, display: 'flex'}}>
+                <div style={{alignSelf: 'stretch', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+                    <div style={{color: '#C0BBE0', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', wordWrap: 'break-word'}}>Especialidad</div>
+                    <div style={{color: 'rgba(52.87, 46.45, 91.37, 0.80)', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', letterSpacing: 0.32, wordWrap: 'break-word'}}>Pediatria</div>
+                </div>
+                <div style={{alignSelf: 'stretch', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+                    <div style={{color: '#C0BBE0', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', wordWrap: 'break-word'}}>Sure</div>
+                    <div style={{color: 'rgba(52.87, 46.45, 91.37, 0.80)', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', letterSpacing: 0.32, wordWrap: 'break-word'}}>Pediatria</div>
+                </div>
+                <div style={{height: 21, position: 'relative'}}>
+                    <div style={{width: 83.23, left: 0, top: 0, position: 'absolute', color: '#C0BBE0', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', wordWrap: 'break-word'}}>Licencia</div>
+                    <div style={{width: 59.61, left: 290.39, top: 0, position: 'absolute', color: 'rgba(52.87, 46.45, 91.37, 0.80)', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', letterSpacing: 0.64, wordWrap: 'break-word'}}>Datos</div>
+                </div>
+                <div style={{height: 21, position: 'relative'}}>
+                    <div style={{width: 129.10, left: 0, top: 0, position: 'absolute', color: '#C0BBE0', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', wordWrap: 'break-word'}}>Telefono</div>
+                    <div style={{width: 59.61, left: 290.39, top: 0, position: 'absolute', color: 'rgba(52.87, 46.45, 91.37, 0.80)', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', letterSpacing: 0.64, wordWrap: 'break-word'}}>Datos</div>
+                </div>
+                <div style={{width: 350, height: 21, position: 'relative'}}>
+                    <div style={{width: 204.17, left: 0, top: 0, position: 'absolute', color: '#C0BBE0', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', wordWrap: 'break-word'}}>Email</div>
+                    <div style={{width: 59.61, left: 290.39, top: 0, position: 'absolute', color: 'rgba(52.87, 46.45, 91.37, 0.80)', fontSize: 16, fontFamily: 'DM Sans', fontWeight: '500', letterSpacing: 0.64, wordWrap: 'break-word'}}>Datos</div>
+                </div>
+            </div>
+        </div>
+        <div style={{width: 434, height: 78, paddingLeft: 24, paddingRight: 24, background: 'white', borderRadius: 10, overflow: 'hidden', borderTop: '1px #E6E6E6 solid', justifyContent: 'space-between', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{height: 49, paddingLeft: 32, paddingRight: 32, paddingTop: 18, paddingBottom: 18, background: '#42B8C3', borderRadius: 10, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex'}}>
+                <div style={{width: 82, height: 13, color: 'white', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word'}}>Regresa</div>
+            </div>
+            <div style={{width: 146, height: 47, paddingTop: 18, paddingBottom: 18, paddingLeft: 32, paddingRight: 14, background: '#42B8C3', borderRadius: 10, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'flex'}}>
+                <div style={{color: 'white', fontSize: 18, fontFamily: 'Montserrat', fontWeight: '600', wordWrap: 'break-word'}}>Selecionar</div>
+            </div>
+        </div>
+    </div>
+</div> */
